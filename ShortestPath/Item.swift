54220f967e2e565950e2a9a677863eb9ca56e5ad
Sandesh <sardar.sandesh@gmail.com>
2026-02-21 15:13:19 +0530

    Initial Commit

---
+++ ShortestPath/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ShortestPath
+//
+//  Created by Sandesh on 21/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ShortestPathTests/ShortestPathTests.swift
@@ -0,0 +1,19 @@
+//
+//  ShortestPathTests.swift
+//  ShortestPathTests
+//
+//  Created by Sandesh on 21/02/26.
+//
+
+import Testing
+@testable import ShortestPath
+
+struct ShortestPathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
